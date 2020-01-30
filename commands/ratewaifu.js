5049cfb7c235d512cd536f0226a672a39ddaa81a
blackholeproductions <46394932+blackholeproductions@users.noreply.github.com>
2020-01-30 08:19:42 -0600

    make the code not as bad

--- commands/ratewaifu.js
+++ commands/ratewaifu.js
@@ -1,23 +1,22 @@
 const util = require("./util/util.js"),
       md5  = require("md5");
-// from old bot, maybe rewrite later
+// from old bot, maybe rewrite later (now partially rewritten)
 const execute = (message, command) => {
   if (command.getArgs().length == 1) {
-    var shipVal = 0;
-    var denominator = 10;
-    var myniggas = command.getArgs()[0]; // WHY THE HELL DID PAST ME NAME THIS VARIABLE THIS
-    var shipa = util.hashCode(myniggas.toLowerCase(), 5812).toString();
-    var shipb = util.hashCode(shipa);
-    var shipc = md5(shipb);
-    var ship = util.hashCode(shipc).toString();
+    var denominator = 10; // What to rate out of (currently 8/10)
+    var waifu = command.getArgs()[0]; 
+    
+    //positively FUCK it into a number. maybe put it in a function later for positively fucking other numbers with relative ease
+    var ship = util.hashCode(md5(util.hashCode(util.hashCode(waifu.toLowerCase(), 5812).toString()))).toString(); 
     var shipd = util.hashCode(ship).toString();
-
-    var shipPercentA = ship.substring(ship.length - 3, ship.length - 1);
+    
+    var shipPercentA = ship.substring(ship.length - 3, ship.length - 1); // Get the rating from the giant number
+    // no idea what this does but i'm pretty sure it makes it 6/9 sometimes or something. dunno.
     if (parseInt(shipd.substring(0, 1)) == 5 && parseInt(shipPercentA) == 6) {
       denominator = 9;
     }
     if (shipPercentA !== "10") shipPercentA = ship.substring(ship.length - 2, ship.length - 1);
-    console.log(myniggas + " got a " + shipPercentA + "/" + denominator);
+    console.log(waifu + " got a " + shipPercentA + "/" + denominator);
     if (shipPercentA !== "8") message.channel.send("i would give `" + command.getArgs()[0] + "` a **" + shipPercentA + "/" + denominator + "**");
     if (shipPercentA == "8") message.channel.send("i would give `" + command.getArgs()[0] + "` an **" + shipPercentA + "/" + denominator + "**");
   }
